ed63c401bbba9d5f4749bfc5c99bdbbeba86444f
MostafaSaad1987 <52860974+MostafaSaad1987@users.noreply.github.com>
2023-05-10 00:24:17 +0300

    Final version for now.

--- script.js
+++ script.js
@@ -1,32 +1,16 @@
-/*
-Write a NEW function called game(). Use the previous function inside of this one to
-play a 5 round game that keeps score and reports a winner or loser at the end.
-You have not officially learned how to “loop” over code to repeat function calls…
-if you already know about loops from somewhere else (or if you feel like doing some more learning)
-feel free to use them. If not, don’t worry! Just call your playRound function 5 times in a row. Loops are covered in the next lesson.
-At this point you should be using console.log() to display the results of each round and the winner at the end.
-Use prompt() to get input from the user. Read the docs here if you need to.
-Feel free to re-work your previous functions if you need to. Specifically, you might want to change the return value to something more useful.
-Feel free to create more “helper” functions if you think it would be useful.
-*/
-
 let choices = ["Rock", "Paper", "Scissors"];
 let playerInput = "";
 let properInput = "";
+let whoWon = "None";
 
-// Code for player input through prompt.
-/*
-do {
-    playerInput = prompt("Please enter your weapon of choice.").toLowerCase();
-    properInput = playerInput.charAt(0).toUpperCase() + playerInput.slice(1);
-} while (!choices.includes(properInput))
+function getUserInput() {
+    do {
+        playerInput = prompt("Please enter your weapon of choice.").toLowerCase();
+        properInput = playerInput.charAt(0).toUpperCase() + playerInput.slice(1);
+    } while (!choices.includes(properInput))
 
-if (choices.includes(properInput)) {
-    playRound(properInput, getComputerChoice());
-} else {
-    console.log("Incorrect input.");
+    return properInput;
 }
-*/
 
 function getComputerChoice() {
     let choiceInt = Math.floor(Math.random() * 3);
@@ -37,14 +21,45 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
 
     if (playerSelection == computerSelection) {
+        whoWon = "None";
         return "Tie. " + playerSelection + " is the same as " + computerSelection;
     } else if ((playerSelection == "Scissors" && computerSelection == "Paper") || (playerSelection == "Paper" && computerSelection == "Rock") || (playerSelection == "Rock" && computerSelection == "Scissors")) {
+        whoWon = "Player";
         return "You Win! " + playerSelection + " beats " + computerSelection;
     } else {
+        whoWon = "Computer";
         return "You Lose! " + computerSelection + " beats " + playerSelection;
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    let computerWins = 0;
+    let playerWins = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let user = getUserInput();
+        let computer = getComputerChoice();
+        let message = playRound(user, computer);
+
+        switch (whoWon) {
+            case "Player":
+                playerWins++;
+                break;
+            case "Computer":
+                computerWins++;
+                break;
+        }
+
+        console.log(message);
+    }
+
+    if (computerWins > playerWins) {
+        console.log("Game ended. Computer wins.")
+    } else if (playerWins > computerWins) {
+        console.log("Game ended. Player wins.")
+    } else {
+        console.log("Game ended. Tie.")
+    }
+}
+
+game();
